31d56d6e6e8b18c14f85d8453240b404eae55d57
dngst <dngst@protonmail.com>
2023-09-14 21:15:18 +0300

    Add caching

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
-    @pagy, @movies = pagy(Movie.all.reverse_order)
+    ids = Rails.cache.fetch('movie_ids', expires_in: 12.hours) do
+      Movie.pluck(:id)
+    end
+    @pagy, @movies = pagy(Movie.where(id: ids).order('created_at DESC'))
   end
 end
