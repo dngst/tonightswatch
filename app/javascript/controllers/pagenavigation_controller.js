6b864dfe59bbb8ed1cdeb375e7411da82a8fa2a2
dngst <dngst@protonmail.com>
2024-02-04 09:56:34 +0300

    Navigate to the next page with space bar

--- app/javascript/controllers/pagenavigation_controller.js
+++ app/javascript/controllers/pagenavigation_controller.js
@@ -3,22 +3,27 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="pagenavigation"
 export default class extends Controller {
   connect() {
-    console.log("Page navigation controller loaded.")
   }
 
   handleKeyDown(event){
     switch (event.key) {
       case "ArrowRight":
         const nextLink = document.querySelector('a[rel="next"]');
-          if (nextLink) {
-            nextLink.click();
-          }
+        if (nextLink) {
+          nextLink.click();
+        }
+        break;
+      case " ":
+        const altnextLink = document.querySelector('a[rel="next"]');
+        if (altnextLink) {
+          altnextLink.click();
+        }
         break;
       case "ArrowLeft":
         const prevLink = document.querySelector('a[rel="prev"]');
-          if (prevLink) {
-            prevLink.click();
-          }
+        if (prevLink) {
+          prevLink.click();
+        }
         break;
       default:
         break;
