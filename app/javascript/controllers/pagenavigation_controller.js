07fad22caa6fba3469024c01e9ed8c284585df96
dngst <dngst@protonmail.com>
2024-04-06 11:37:44 +0300

    Correct switch

--- app/javascript/controllers/pagenavigation_controller.js
+++ app/javascript/controllers/pagenavigation_controller.js
@@ -2,31 +2,26 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="pagenavigation"
 export default class extends Controller {
-  connect() {
-  }
-
-  handleKeyDown(event){
-    switch (event.key) {
-      case "ArrowRight":
-        const nextLink = document.querySelector('a[rel="next"]');
-        if (nextLink) {
-          nextLink.click();
-        }
-        break;
-      case " ":
-        const altnextLink = document.querySelector('a[rel="next"]');
-        if (altnextLink) {
-          altnextLink.click();
-        }
-        break;
-      case "ArrowLeft":
-        const prevLink = document.querySelector('a[rel="prev"]');
-        if (prevLink) {
-          prevLink.click();
-        }
-        break;
-      default:
-        break;
+    connect() {
     }
-  }
+
+    handleKeyDown(event){
+        const nextLink = document.querySelector('a[rel="next"]');
+        const prevLink = document.querySelector('a[rel="prev"]');
+        switch (event.key) {
+            case "ArrowRight":
+            case " ":
+                if (nextLink) {
+                    nextLink.click();
+                }
+                break;
+            case "ArrowLeft":
+                if (prevLink) {
+                    prevLink.click();
+                }
+                break;
+            default:
+                break;
+        }
+    }
 }
