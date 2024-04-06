c02fe7806a4e95af416dc80de5441082b3634268
dngst <dngst@protonmail.com>
2024-04-06 11:48:30 +0300

    Add h and l support

--- app/javascript/controllers/pagenavigation_controller.js
+++ app/javascript/controllers/pagenavigation_controller.js
@@ -10,12 +10,14 @@ export default class extends Controller {
         const prevLink = document.querySelector('a[rel="prev"]');
         switch (event.key) {
             case "ArrowRight":
+            case "l":
             case " ":
                 if (nextLink) {
                     nextLink.click();
                 }
                 break;
             case "ArrowLeft":
+            case "h":
                 if (prevLink) {
                     prevLink.click();
                 }
